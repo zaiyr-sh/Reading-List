9836ae8c48fa590c7dfdca5f7d0ba8e1098f9e88
Zaiyr <zaiyr.00@gmail.com>
2020-02-13 21:36:25 +0600

    Reading List

---
+++ src/main/java/readinglist/ReadingListController.java
@@ -0,0 +1,37 @@
+package readinglist;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+import java.util.List;
+
+@Controller
+public class ReadingListController {
+    private ReadingListRepository readingListRepository;
+
+    @Autowired
+    public ReadingListController(ReadingListRepository readingListRepository) {
+        this.readingListRepository = readingListRepository;
+    }
+
+
+    @GetMapping(value = "/")
+    public String readersBooks(String reader, Model model) {
+        List<Book> readingList = readingListRepository.findByReader(reader);
+        if (readingList != null) {
+            model.addAttribute("books", readingList);
+        }
+        return "readingList";
+    }
+
+    @PostMapping(value = "/")
+    public String addToReadingList( String reader, Book book) {
+        book.setReader(reader);
+        readingListRepository.save(book);
+        return "redirect:/";
+    }
+}
